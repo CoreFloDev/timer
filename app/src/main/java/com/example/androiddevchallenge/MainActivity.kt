94ce84fb40895d5813743b66011d1aa5f37b95e6
CoreFloDev <CoreFloDev@users.noreply.github.com>
2021-03-08 23:34:24 +0000

    prepare for challenge

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,13 +20,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -52,6 +68,7 @@ class MainActivity : AppCompatActivity() {
             MyTheme {
                 MyApp(
                     onClickedOnButton = mainViewModel::buttonClicked,
+                    scope = mainViewModel.scope,
                     startedCounterTime = mainViewModel.triggerStartTime
                 )
             }
@@ -63,7 +80,8 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp(
     onClickedOnButton: () -> Unit = {},
-    startedCounterTime: ZonedDateTime? = null
+    startedCounterTime: ZonedDateTime? = null,
+    scope: CoroutineScope = CoroutineScope(Dispatchers.Main)
 ) {
     Surface(color = MaterialTheme.colors.background) {
         Column(
@@ -85,7 +103,7 @@ fun MyApp(
             if (startedCounterTime != null) {
                 val targetTime = startedCounterTime.plusSeconds(59)
 
-                CoroutineScope(Dispatchers.Main).launch {
+                scope.launch {
                     do {
                         val secondLeft = ChronoUnit.SECONDS.between(ZonedDateTime.now(), targetTime)
                         timeText = "00:00:$secondLeft.000"
@@ -127,7 +145,6 @@ fun MyApp(
                 }
             }
         }
-
     }
 }
 
